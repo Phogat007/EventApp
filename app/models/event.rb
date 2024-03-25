aabf386c44d146f9f2185721c20d5d789deb0879
Bhavyansh <bhavyansh001@gmail.com>
2024-03-25 17:12:01 +0530

    Taking site online hopefully

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,14 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :event_photos, dependent: :destroy
   has_many_attached :event_photos
+
+  validate :limit_photos_to_four
+
+  private
+
+  def limit_photos_to_four
+    if event_photos.attached? && event_photos.count > 4
+      errors.add(:event_photos, "You can only upload up to 4 photos")
+    end
+  end
 end

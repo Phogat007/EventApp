e6108e2d5c7963f392b0e27afdac9e3a33b72da1
Bhavyansh <bhavyansh001@gmail.com>
2024-04-10 17:04:44 +0530

    removed fields of devise for functionality

---
+++ db/migrate/20240410113253_remove_confirmable_fields_from_users.rb
@@ -0,0 +1,8 @@
+class RemoveConfirmableFieldsFromUsers < ActiveRecord::Migration[7.1]
+  def change
+     remove_column :users, :unconfirmed_email, :string
+     remove_column :users, :confirmation_token, :string
+     remove_column :users, :confirmed_at, :datetime
+     remove_column :users, :confirmation_sent_at, :datetime
+  end
+ end

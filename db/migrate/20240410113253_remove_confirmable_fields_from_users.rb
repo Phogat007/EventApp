7d8f1dd3d1441882b654f89b8a7e628fedb30683
Phogat007 <mphogat007@gmail.com>
2024-04-11 20:25:48 +0530

    final

--- db/migrate/20240410113253_remove_confirmable_fields_from_users.rb
+++ db/migrate/20240410113253_remove_confirmable_fields_from_users.rb
@@ -1,8 +1,8 @@
-class RemoveConfirmableFieldsFromUsers < ActiveRecord::Migration[7.1]
-  def change
-     remove_column :users, :unconfirmed_email, :string
-     remove_column :users, :confirmation_token, :string
-     remove_column :users, :confirmed_at, :datetime
-     remove_column :users, :confirmation_sent_at, :datetime
-  end
+class RemoveConfirmableFieldsFromUsers < ActiveRecord::Migration[6.0]
+   def change
+     if column_exists?(:users, :unconfirmed_email)
+       remove_column :users, :unconfirmed_email, :string
+     end
+   end
  end
+ 

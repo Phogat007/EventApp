ac9506364063127288f667fbb0444590170e05ad
Bhavyansh <bhavyansh001@gmail.com>
2024-04-12 12:14:10 +0530

    fixing turbo errors

--- app/javascript/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  
-
- // alertCloseBtn.onclick = function () {
- //   console.log('fired');
- //   document.getElementById("alert").style.display = "none";
- // }
-});

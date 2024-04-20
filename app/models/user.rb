9fc3ba8250d666ee7b00d9a00b18ef889a7e9569
Bhavyansh <bhavyansh001@gmail.com>
2024-04-20 14:43:59 +0530

    Added tests for User model, model tests complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :events, dependent: :destroy
+  validates :name, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,8 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.unique.email }
+    email { Faker::Internet.email }
     password { "password" }
-    reset_password_token { SecureRandom.hex }
-    reset_password_sent_at { Time.now }
-    remember_created_at { Time.now }
     is_admin { false }
     name { Faker::Name.name }
-    confirmation_token { SecureRandom.hex }
-    confirmed_at { Time.now }
-    confirmation_sent_at { Time.now }
-    unconfirmed_email { nil }
   end
 end

4d97cfd3efeb4d326dfb28529ddeb2db10caba91
Bhavyansh <bhavyansh001@gmail.com>
2024-04-20 17:31:54 +0530

    Added routing tests, all passing

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
   resources :events do
     member do
       get 'download'
